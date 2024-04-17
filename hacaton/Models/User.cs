1c2ea955b6a8f3ba507ca2ca9f3097365c6cca38
Askarbekov Aktan <akn392204@gmail.com>
2024-04-17 15:56:20 +0600

    Add models

--- hacaton/Models/User.cs
+++ hacaton/Models/User.cs
@@ -7,6 +7,5 @@ public class User
     public string LastName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public int WorkId { get; set; }
-    public Work Work { get; set; }
+  
 }
--- hacaton/Models/Work.cs
+++ hacaton/Models/Work.cs
@@ -3,10 +3,15 @@ namespace hacaton.Models;
 public class Work
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string Name { get; set; }
+    public string Value { get; set; }
+    public int Like { get; set; }
+    public int Dislike { get; set; }
+    
     public int UserId { get; set; }
     public User User { get; set; }
     public int CategoryId { get; set; }
     public Category Category { get; set; }
-    public string Value { get; set; }
+    public int CommentId { get; set; }
+    public Comment Comment { get; set; }
 }
